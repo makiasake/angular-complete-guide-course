293547c5fedd00d9754312446f5991f15e115502
makiasake <alexandre.maki@hotmail.com>
2022-09-19 11:55:30 -0300

    Creating components with the angular cli & nesting components

---
+++ angular-the-complete-guide-course/src/app/servers/servers.component.ts
@@ -0,0 +1,20 @@
+/* 
+  Component generated with CLI
+  run: $ ng generate component servers or run: $ ng g c servers
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
